f2f5207f3b215b169d10de297aac8cb410b9fdf0
bearddan2000 <bearddan2000@yahoo.com>
2023-09-16 00:52:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# java-web-maven-spring-thyme-oracle-simple
+
+## Description
+A POC for springboot web app with thymeleaf support
+connecting to a database.
+
+## Tech stack
+- java
+- hibernate
+- maven
+  - springboot
+  - thymeleaf
+  - oracle connector
+- bootstrap
+- jquery
+- dataTable
+
+## Docker stack
+- gvenzl/oracle-free:latest
+- maven:3-openjdk-17
+
+## To run
+`sudo ./install.sh -u`
+
+[Available](http://localhost)
+
+## To stop
+`sudo ./install.sh -d`
+
+## For help
+`sudo ./install.sh -h`
---
+++ java-srv/bin/src/main/java/example/controller/DogRestController.java
@@ -0,0 +1,36 @@
+package example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+import example.model.Dog;
+import example.repository.DogRepository;
+
+@RestController
+public class DogRestController {
+
+	@Autowired
+	private DogRepository dogRepository;
+
+	@PostConstruct
+	private void init()
+	{
+	    List<Dog> list = new ArrayList<Dog>() {{
+		    add(new Dog(1, "Am Bulldog", "White"));
+		    add(new Dog(2, "Foxhound", "Red"));
+		    add(new Dog(3, "Gr Shepard", "Black"));
+			}};
+	
+		list.forEach(dog -> {dogRepository.save(dog);});
+	}
+
+	@RequestMapping(path="/dogs", method=RequestMethod.GET)
+	public Iterable<Dog> getAllDogs(){
+		return dogRepository.findAll();
+	}
+}
